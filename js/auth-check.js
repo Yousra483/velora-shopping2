c99b0ed6d25ba77eeb0613d2c9184f865bd0fd27
Yousra Mustamandi <mustamandiyousra8@gmail.com>
2025-10-21 17:10:34 +0200

    Update auth-check.js

--- js/auth-check.js
+++ js/auth-check.js
@@ -1,19 +1,5 @@
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const path = window.location.pathname;
 
-
-//     const isLoginPage = path.endsWith('/index.html') || path.endsWith('/velora-shopping2/') || path === '/velora-shopping2/';
-
-
-//     if (sessionStorage.getItem('isLoggedIn') !== 'true' && !isLoginPage) {
-//         window.location.href = '/velora-shopping2/index.html';
-//     }
-
-//     if (sessionStorage.getItem('isLoggedIn') === 'true' && isLoginPage) {
-//         window.location.href = '/velora-shopping2/home.html';
-//     }
-// });
 
 
 // js/auth-check.js
